bcaecc0642faace1ef88302cc1e7c6b9f5d27fa4
Mark Hernandez <m.hern1415@gmail.com>
2019-08-30 21:03:21 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -16,8 +16,7 @@ def select_name_and_series_subgenres_of_authors
 end
 
 def select_series_title_with_most_human_characters
-      "SELECT title FROM (
-    SELECT series.title AS title, COUNT(characters.series_id) AS num_species
+      "SELECT title FROM (SELECT series.title AS series_title, COUNT(characters.series_id) AS species_count
       FROM series
       INNER JOIN characters
         ON series.id = characters.series_id
