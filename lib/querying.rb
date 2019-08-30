68a298e1c4739f4cb6937e2025042d7cee73230d
Mark Hernandez <m.hern1415@gmail.com>
2019-08-30 21:06:29 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -17,7 +17,7 @@ end
 
 def select_series_title_with_most_human_characters
       "SELECT title FROM (
-        SELECT series.title AS title, COUNT(character.series_id) AS species_count FROM series INNER JOIN characters ON series.id = characters.series_id WHERE characters.species = 'human' GROUP BY characters.series_id ORDER BY species_count DESC LIMIT 1);"
+        SELECT series.title AS title, COUNT(character .series_id) AS species_count FROM series INNER JOIN characters ON series.id = characters.series_id WHERE characters.species = 'human' GROUP BY characters.series_id ORDER BY species_count DESC LIMIT 1);"
 end
 
 
